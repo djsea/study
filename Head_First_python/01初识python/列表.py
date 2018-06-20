f18f0f065d8164a3e63af2ff36da89818041f2a8
djsea <djsea@139.com>
2018-06-20 23:20:01 +0800

    aa

---
+++ Head_First_python/01初识python/列表.py
@@ -0,0 +1,14 @@
+movies = [
+    "The Holy Grail",
+    "The Life of Brian",
+    "The Meaning of Life"]
+
+# print(movies[1])
+# print(len(movies))
+# movies.append("xindianying")
+# print(movies)
+# movies.extend(["xin1", "xin2"])
+# print(movies)
+# movies.remove("xin1")
+# print(movies)
+
